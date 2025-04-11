bf8aa9c71c33527390fdfd78ba97520619ae4c14
Yavierg <ygonzalez0936@stu.nuc.edu>
2025-04-11 16:20:49 -0400

    Corrige duplicados en atributos Assembly para evitar errores de compilación

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("WebSimpleASPNet48")]
+// [assembly: AssemblyTitle("WebSimpleASPNet48")]
+// [assembly: AssemblyConfiguration("")]
+// [assembly: AssemblyCompany("")]
+// [assembly: AssemblyProduct("WebSimpleASPNet48")]
+// [assembly: AssemblyVersion("1.0.0.0")]
+// [assembly: AssemblyFileVersion("1.0.0.0")]
+
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebSimpleASPNet48")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a1a1a1a1-a1a1-4a1a-b1a1-a1a1a1a1a1a1")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
